4efe46e82e28d2be60b69858cabe728e69f5938f
Junho Lee <palejelly@gmail.com>
2022-02-17 00:29:08 -0500

    Major_upload Still major_industry not working

--- config.js
+++ config.js
@@ -16,66 +16,129 @@ var config = {
             description: 'Facts about prisons nyc',
             location: {
                 center: [-75.922,42.347],
-                zoom: 7.3,
+                zoom: 7.1,
                 pitch: 0,
                 bearing: 0.00
             },
             onChapterEnter: [
-                {layer: 'food', opacity: 0 },
                 {layer: 'census', opacity: 0 },
+                {layer: 'major_industry', opacity: 1.0},
                 {layer: 'prison_text', opacity: 1 },
-                {layer: 'prison', opacity: 0.0 },
-                {layer: 'satellite', opacity:0.5 },
+                {layer: 'prison', opacity: 0.8 },
+                {layer: 'satellite', opacity: 0.5 },
+                {layer: 'Historical_population', opacity:0 },
+                {layer: 'Population_at_closing', opacity:0 },
+                {layer: 'Population_at_opening', opacity:0 },
             ],
+
             onChapterExit: [
               
             ]
         },
         {
             id: 'chapter2',
-            alignment: 'left',
-            title: 'System Diagram',
-            image: '',
-            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            alignment: 'fixed_left',
+            title: 'Hitorical Population (1870)',
+            image: 'src/Bar Graph - 1870.png',
+            description: '',
             location: {
                 center: [-75.922,42.347],
-                zoom: 7.3,
+                zoom: 7.1,
                 pitch: 0,
                 bearing: 0.00
             },
             onChapterEnter: [
-                {layer: 'food', opacity: 0 },
-                {layer: 'census', opacity: 1 },
-                {layer: 'prison_text', opacity: 1.0 },
-                {layer: 'prison', opacity:0 },
-                {layer: 'satellite', opacity:0.0 },
+                {layer: 'census', opacity: 0 },
+                {layer: 'major_industry', opacity: 0},
+                {layer: 'prison_text', opacity: 1 },
+                {layer: 'prison', opacity: 0.8 },
+                {layer: 'satellite', opacity: 0.5 },
+                {layer: 'Historical_population', opacity:1.0 },
+                {layer: 'Population_at_closing', opacity:0 },
+                {layer: 'Population_at_opening', opacity:0 },
             ],
+
             onChapterExit: [
               
             ]
         },
         {
             id: 'chapter3',
-            alignment: 'right',
-            title: 'Chapter 3',
-            image: 'images/9_2420_Cattlyst_Vig1.jpg',
+            alignment: 'fixed_left',
+            title: 'Historical Population at Opening',
+            image: 'src/Bar Graph - Opening.png',
             description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
             location: {
                 center: [-75.922,42.347],
-                zoom: 7.3,
+                zoom: 7.1,
                 pitch: 0,
                 bearing: 0.00
             },
             onChapterEnter: [
-                {layer: 'food', opacity: 1 },
                 {layer: 'census', opacity: 0 },
-                {layer: 'prison_text', opacity: 0.0 },
-                {layer: 'prison', opacity:1.0 },
-                {layer: 'satellite', opacity:0.5 },
+                {layer: 'major_industry', opacity: 0},
+                {layer: 'prison_text', opacity: 1 },
+                {layer: 'prison', opacity: 0.8 },
+                {layer: 'satellite', opacity: 0.5 },
+                {layer: 'Historical_population', opacity:0 },
+                {layer: 'Population_at_closing', opacity:0 },
+                {layer: 'Population_at_opening', opacity:1.0 },
             ],
             onChapterExit: [
               
             ]
-        }
+        },
+        {
+            id: 'chapter4',
+            alignment: 'fixed_left',
+            title: 'Historical Population at Closing',
+            image: 'src/Bar Graph - Closing.png',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [-75.922,42.347],
+                zoom: 7.1,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'census', opacity: 0 },
+                {layer: 'major_industry', opacity: 0},
+                {layer: 'prison_text', opacity: 1 },
+                {layer: 'prison', opacity: 0.8 },
+                {layer: 'satellite', opacity: 0.5 },
+                {layer: 'Historical_population', opacity:0 },
+                {layer: 'Population_at_closing', opacity:1.0 },
+                {layer: 'Population_at_opening', opacity:0 },
+            ],
+            onChapterExit: [
+              
+            ]
+        },
+        {
+            id: 'chapter5',
+            alignment: 'fixed_left',
+            title: 'Industry',
+            image: '',
+            description: 'uam erat volutpat. Sed ullamcorper convallis eros ut auctor. Cras vel iaculis ligula, et vestibulum metus.',
+            location: {
+                center: [-75.922,42.347],
+                zoom: 7.1,
+                pitch: 0,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {layer: 'census', opacity: 0 },
+                {layer: 'major_industry', opacity: 1.0},
+                {layer: 'prison_text', opacity: 0.0 },
+                {layer: 'prison', opacity: 0.8 },
+                {layer: 'satellite', opacity: 0.5 },
+                {layer: 'Historical_population', opacity:0 },
+                {layer: 'Population_at_closing', opacity:0 },
+                {layer: 'Population_at_opening', opacity:0 },
+            ],
+            onChapterExit: [
+              
+            ]
+        },
     ]
 };
