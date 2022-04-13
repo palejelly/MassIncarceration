268885d06cbc4ba6cb35613b6159b95914c5419c
JunhoLee <jl5992@columbia.edu>
2022-04-13 18:57:21 -0400

    huge change, added alot

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 var tile_frame= document.querySelector("#tile_frame.slide");
   
+var largemap_1= document.querySelector("#largemap_1");
 
 
 //--------------------------------- tiling part starts --------------------------------
@@ -325,21 +326,45 @@ const only_closed_checkbox = document.querySelector('input[id="only_closed"]');
 // ----------------------tiling part ended.
 
 
+let ticking = false;
 
+function foo() {
+    if (!ticking) {
+        ticking = true;
+        requestAnimationFrame(() => {
+            console.log("Scrolled!");
+            ticking = false;
+        });
+    }
+}
 
-document.addEventListener('scroll', function(e) {
+window.addEventListener("scroll", foo, { passive: true });
+
+document.addEventListener('scroll', function(e){
     // console.log(window.scrollY);
 
+    if(window.scrollY>innerHeight*6){
+        largemap_1.style.opacity = 0;
 
-    if(window.scrollY>innerHeight*5){
+
+    }else if(window.scrollY>innerHeight*5){
         console.log("map out");
+        tile_frame.style.opacity = 0;
+
+        tile_frame.style.display = "none";
         tile_frame.classList.add("fade_out");
+        
+
+        largemap_1.style.opacity = 1;
         // tile_frame.classList.remove("scroll_locked");
 
     }else if (window.scrollY>innerHeight*4) {
         console.log("thrid");
+        tile_frame.style.display = "block";
+
         scroll_progress = (window.scrollY-innerHeight*4)/innerHeight;
-        // tile_frame.style.opacity = 1-scroll_progress;
+        largemap_1.style.opacity = scroll_progress;
+        tile_frame.style.opacity = 1-scroll_progress;
         for(let i in tilelist){
 
             tilelist[i].style.top=format("{0}px",(destXY_tileWindowCoord[i][1]-departXY_tileWindowCoord[i][1])*scroll_progress);
@@ -352,8 +377,6 @@ document.addEventListener('scroll', function(e) {
 
             tilelist[i].style.transform = format("scale({0})", 1-scroll_progress);
 
-            // tilelist[i].style.top=format("{0}px",50);
-            // tilelist[i].style.left=format("{0}px",10);
         }
     }else if (window.scrollY>innerHeight*3) {
         console.log("second");
@@ -368,21 +391,19 @@ document.addEventListener('scroll', function(e) {
         }
 
     }else if (window.scrollY>innerHeight*2) {
-        // console.log("map reached");
-        acquireTileLocation([-80.01588907134725, 40.61072561851073, -72.89488934527353, 42.26025415432661]);
+        acquireTileLocation([-80.535294, 40.244927, -66.533218, 45.347304]);
         tile_frame.classList.add("scroll_locked");
-        tile_frame.classList.remove("fade_out");
         for(let i in open_prison_div){
             open_prison_div[i].classList.remove("open");
         }
     }else{
-        if (!tile_frame.classList.contains("fade_out")){
-                tile_frame.classList.add("fade_out");
-        }
+        // if (!tile_frame.classList.contains("fade_out")){
+        //         tile_frame.classList.add("fade_out");
+        // }
     }
 
 
-  });
+  },{ passive: true });
 
 
 
@@ -410,183 +431,194 @@ function acquireTileLocation(boundinglnglat){
 
 
 
-var story = document.getElementById('story');
-var features = document.createElement('div');
-features.setAttribute('id', 'features');
+// var story = document.getElementById('story');
+// var features = document.createElement('div');
+// features.setAttribute('id', 'features');
 
-var header = document.createElement('div');
+// var header = document.createElement('div');
 
-if (config.title) {
-    var titleText = document.createElement('h1');
-    titleText.innerText = config.title;
-    header.appendChild(titleText);
-}
+// if (config.title) {
+//     var titleText = document.createElement('h1');
+//     titleText.innerText = config.title;
+//     header.appendChild(titleText);
+// }
 
-if (config.subtitle) {
-    var subtitleText = document.createElement('h2');
-    subtitleText.innerText = config.subtitle;
-    header.appendChild(subtitleText);
-}
+// if (config.subtitle) {
+//     var subtitleText = document.createElement('h2');
+//     subtitleText.innerText = config.subtitle;
+//     header.appendChild(subtitleText);
+// }
 
-if (config.byline) {
-    var bylineText = document.createElement('p');
-    bylineText.innerText = config.byline;
-    header.appendChild(bylineText);
-}
+// if (config.byline) {
+//     var bylineText = document.createElement('p');
+//     bylineText.innerText = config.byline;
+//     header.appendChild(bylineText);
+// }
 
-if (header.innerText.length > 0) {
-    header.classList.add(config.theme);
-    header.setAttribute('id', 'header');
-    story.appendChild(header);
-}
+// if (header.innerText.length > 0) {
+//     header.classList.add(config.theme);
+//     header.setAttribute('id', 'header');
+//     story.appendChild(header);
+// }
 
-config.chapters.forEach((record, idx) => {
-    var container = document.createElement('div');
-    var chapter = document.createElement('div');
+// config.chapters.forEach((record, idx) => {
+//     var container = document.createElement('div');
+//     var chapter = document.createElement('div');
 
-    if (record.title) {
-        var title = document.createElement('h3');
-        title.innerText = record.title;
-        chapter.appendChild(title);
-    }
+//     if (record.title) {
+//         var title = document.createElement('h3');
+//         title.innerText = record.title;
+//         chapter.appendChild(title);
+//     }
 
-    if (record.image) {
-        var image = new Image();
-        image.src = record.image;
-        chapter.appendChild(image);
-    }
+//     if (record.image) {
+//         var image = new Image();
+//         image.src = record.image;
+//         chapter.appendChild(image);
+//     }
 
-    if (record.description) {
-        var story = document.createElement('p');
-        story.innerHTML = record.description;
-        chapter.appendChild(story);
-    }
+//     if (record.description) {
+//         var story = document.createElement('p');
+//         story.innerHTML = record.description;
+//         chapter.appendChild(story);
+//     }
 
-    container.setAttribute('id', record.id);
-    container.classList.add('step');
-    if (idx === 0) {
-        container.classList.add('active');
-    }
+//     container.setAttribute('id', record.id);
+//     container.classList.add('step');
+//     if (idx === 0) {
+//         container.classList.add('active');
+//     }
 
-    chapter.classList.add(config.theme);
-    container.appendChild(chapter);
-    container.classList.add(alignments[record.alignment] || 'centered');
-    if (record.hidden) {
-        container.classList.add('hidden');
-    }
-    features.appendChild(container);
-});
+//     chapter.classList.add(config.theme);
+//     container.appendChild(chapter);
+//     container.classList.add(alignments[record.alignment] || 'centered');
+//     if (record.hidden) {
+//         container.classList.add('hidden');
+//     }
+//     features.appendChild(container);
+// });
 
-story.appendChild(features);
+// story.appendChild(features);
 
-var footer = document.createElement('div');
+// var footer = document.createElement('div');
 
-if (config.footer) {
-    var footerText = document.createElement('p');
-    footerText.innerHTML = config.footer;
-    footer.appendChild(footerText);
-}
+// if (config.footer) {
+//     var footerText = document.createElement('p');
+//     footerText.innerHTML = config.footer;
+//     footer.appendChild(footerText);
+// }
 
-if (footer.innerText.length > 0) {
-    footer.classList.add(config.theme);
-    footer.setAttribute('id', 'footer');
-    story.appendChild(footer);
-}
+// if (footer.innerText.length > 0) {
+//     footer.classList.add(config.theme);
+//     footer.setAttribute('id', 'footer');
+//     story.appendChild(footer);
+// }
 
-mapboxgl.accessToken = config.accessToken;
+// mapboxgl.accessToken = config.accessToken;
 
-const transformRequest = (url) => {
-    const hasQuery = url.indexOf("?") !== -1;
-    const suffix = hasQuery ? "&pluginName=scrollytellingV2" : "?pluginName=scrollytellingV2";
+// const transformRequest = (url) => {
+//     const hasQuery = url.indexOf("?") !== -1;
+//     const suffix = hasQuery ? "&pluginName=scrollytellingV2" : "?pluginName=scrollytellingV2";
 
-    return {
-      url: url + suffix
-    }
-}
+//     return {
+//       url: url + suffix
+//     }
+// }
 
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: config.style,
-    center: config.chapters[0].location.center,
-    zoom: config.chapters[0].location.zoom,
-    bearing: config.chapters[0].location.bearing,
-    pitch: config.chapters[0].location.pitch,
-    interactive: false,
-    transformRequest: transformRequest
-});
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     style: config.style,
+//     center: config.chapters[0].location.center,
+//     zoom: config.chapters[0].location.zoom,
+//     bearing: config.chapters[0].location.bearing,
+//     pitch: config.chapters[0].location.pitch,
+//     interactive: false,
+//     transformRequest: transformRequest
+// });
 
-if (config.showMarkers) {
-    var marker = new mapboxgl.Marker({ color: config.markerColor });
-    marker.setLngLat(config.chapters[0].location.center).addTo(map);
-}
+// if (config.showMarkers) {
+//     var marker = new mapboxgl.Marker({ color: config.markerColor });
+//     marker.setLngLat(config.chapters[0].location.center).addTo(map);
+// }
 
-// instantiate the scrollama
-var scroller = scrollama();
+// // instantiate the scrollama
+// var scroller = scrollama();
 
-map.on("load", function() {
+// map.on("load", function() {
     
-    if (config.use3dTerrain) {
-        map.addSource('mapbox-dem', {
-            'type': 'raster-dem',
-            'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
-            'tileSize': 512,
-            'maxzoom': 14
-        });
-        // add the DEM source as a terrain layer with exaggerated height
-        map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+//     if (config.use3dTerrain) {
+//         map.addSource('mapbox-dem', {
+//             'type': 'raster-dem',
+//             'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+//             'tileSize': 512,
+//             'maxzoom': 14
+//         });
+//         // add the DEM source as a terrain layer with exaggerated height
+//         map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
 
-        // add a sky layer that will show when the map is highly pitched
-        map.addLayer({
-            'id': 'sky',
-            'type': 'sky',
-            'paint': {
-                'sky-type': 'atmosphere',
-                'sky-atmosphere-sun': [0.0, 0.0],
-                'sky-atmosphere-sun-intensity': 15
-            }
-        });
-    };
+//         // add a sky layer that will show when the map is highly pitched
+//         map.addLayer({
+//             'id': 'sky',
+//             'type': 'sky',
+//             'paint': {
+//                 'sky-type': 'atmosphere',
+//                 'sky-atmosphere-sun': [0.0, 0.0],
+//                 'sky-atmosphere-sun-intensity': 15
+//             }
+//         });
+//     };
 
-    // setup the instance, pass callback functions
-    scroller
-    .setup({
-        step: '.step',
-        offset: 0.7,
-        progress: true
-    })
-    .onStepEnter(response => {
-        var chapter = config.chapters.find(chap => chap.id === response.element.id);
-        response.element.classList.add('active');
-        map[chapter.mapAnimation || 'flyTo'](chapter.location);
+//     // setup the instance, pass callback functions
+//     scroller
+//     .setup({
+//         step: '.step',
+//         offset: 0.7,
+//         progress: true
+//     })
+//     .onStepEnter(response => {
+//         var chapter = config.chapters.find(chap => chap.id === response.element.id);
+//         response.element.classList.add('active');
+//         map[chapter.mapAnimation || 'flyTo'](chapter.location);
 
-        if (config.showMarkers) {
-            marker.setLngLat(chapter.location.center);
-        }
-        if (chapter.onChapterEnter.length > 0) {
-            chapter.onChapterEnter.forEach(setLayerOpacity);
-        }
-        if (chapter.callback) {
-            window[chapter.callback]();
-        }
-        if (chapter.rotateAnimation) {
-            map.once('moveend', function() {
-                const rotateNumber = map.getBearing();
-                map.rotateTo(rotateNumber + 90, {
-                    duration: 24000, easing: function (t) {
-                        return t;
-                    }
-                });
-            });
-        }
-    })
-    .onStepExit(response => {
-        var chapter = config.chapters.find(chap => chap.id === response.element.id);
-        response.element.classList.remove('active');
-        if (chapter.onChapterExit.length > 0) {
-            chapter.onChapterExit.forEach(setLayerOpacity);
-        }
-    });
-});
+//         if (config.showMarkers) {
+//             marker.setLngLat(chapter.location.center);
+//         }
+//         if (chapter.onChapterEnter.length > 0) {
+//             chapter.onChapterEnter.forEach(setLayerOpacity);
+//         }
+//         if (chapter.callback) {
+//             window[chapter.callback]();
+//         }
+//         if (chapter.rotateAnimation) {
+//             map.once('moveend', function() {
+//                 const rotateNumber = map.getBearing();
+//                 map.rotateTo(rotateNumber + 90, {
+//                     duration: 24000, easing: function (t) {
+//                         return t;
+//                     }
+//                 });
+//             });
+//         }
+//     })
+//     .onStepExit(response => {
+//         var chapter = config.chapters.find(chap => chap.id === response.element.id);
+//         response.element.classList.remove('active');
+//         if (chapter.onChapterExit.length > 0) {
+//             chapter.onChapterExit.forEach(setLayerOpacity);
+//         }
+//     });
+// });
 
 // setup resize event
-window.addEventListener('resize', scroller.resize);
+// window.addEventListener('resize', scroller.resize);
+window.onbeforeunload = function (e) {
+    var e = e || window.event;
+
+    // For IE and Firefox
+    if (e) {
+        e.returnValue = 'Leaving the page';
+    }
+
+    // For Safari
+    return 'Leaving the page';
+};
